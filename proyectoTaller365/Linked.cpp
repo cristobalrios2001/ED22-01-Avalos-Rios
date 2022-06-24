d4261556213537a81085da2c1cfafc6a56461608
CristopherA27 <76230505+CristopherA27@users.noreply.github.com>
2022-06-23 23:09:58 -0400

    funciones nodo

--- proyectoTaller365/Linked.cpp
+++ proyectoTaller365/Linked.cpp
@@ -14,17 +14,43 @@ public:
     LinkedList() {
         first = NULL;
     }
-
     void addNode(Persona persona) {
-        Nodo* nuevo = new Nodo;
-        nuevo->setPersona(persona);
-        nuevo->setNext(NULL);
-
-        if (first == NULL) {
-            first = nuevo;
+        Nodo* nodo = new Nodo();
+        nodo->setPersona(persona);
+        nodo->setNext(NULL);
+        if (this->first == NULL) {
+            this->first = nodo;
         }
-
-
+        else {
+            Nodo* current = this->first;
+            while (current->getNext() != NULL) {
+                current = current->getNext();
+            }
+            current->setNext(nodo);
+        }
     }
+    int getSize() {
+        Nodo* aux = this->first;
+        int cont = 0;
+        while (aux->getNext() != NULL) {
+            cont++;
+            aux = aux->getNext();
+        }
+        return cont; 
+    }
+
+    void show() {
+        Nodo* aux = this->first;
+        if (this->first == NULL) {
+            cout << "Lista Vacia" << endl;
+        }
+        else {
+            while (aux != NULL) {
+               // cout << aux->getPersona() << endl; variable
+                aux = aux->getNext();
+            }
+        }
+    }
+
 
 };
